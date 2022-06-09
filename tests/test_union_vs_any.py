ec86997f58a082058843e7fbcaea6fb0e466629c
Niels Lerche Sørensen <nielslerches@gmail.com>
2022-06-09 09:26:33 +0200

    index vs no index

--- tests/test_union_vs_any.py
+++ tests/test_union_vs_any.py
@@ -6,19 +6,33 @@ from .utils import execute_and_measure
 @pytest.mark.parametrize(
     "statements",
     [
+        # Without index
         [
             "CREATE TEMPORARY TABLE {tablename.next} AS SELECT round(random() * 1000) id FROM generate_series(1, 1000000)",
-            "CREATE TEMPORARY TABLE {tablename.next} AS SELECT round(random() * 1000) AS id FROM generate_series(1, 1000000)",
             """
-            SELECT {tablename.history[-2]}.id FROM {tablename.history[-2]} WHERE {tablename.history[-2]}.id = 250
+            SELECT {tablename.history[-1]}.id FROM {tablename.history[-1]} WHERE {tablename.history[-1]}.id = 250
             UNION
             SELECT {tablename.history[-1]}.id FROM {tablename.history[-1]} WHERE {tablename.history[-1]}.id = 750
             """,
         ],
         [
             "CREATE TEMPORARY TABLE {tablename.next} AS SELECT round(random() * 1000) id FROM generate_series(1, 1000000)",
-            "CREATE TEMPORARY TABLE {tablename.next} AS SELECT round(random() * 1000) AS id FROM generate_series(1, 1000000)",
-            "SELECT {tablename.history[-2]}.id FROM {tablename.history[-2]} WHERE {tablename.history[-2]}.id IN (250, 750)",
+            "SELECT {tablename.history[-1]}.id FROM {tablename.history[-1]} WHERE {tablename.history[-1]}.id IN (250, 750)",
+        ],
+        # With index
+        [
+            "CREATE TEMPORARY TABLE {tablename.next} AS SELECT round(random() * 1000) id FROM generate_series(1, 1000000)",
+            "CREATE INDEX ON {tablename.history[-1]} (id)",
+            """
+            SELECT {tablename.history[-1]}.id FROM {tablename.history[-1]} WHERE {tablename.history[-1]}.id = 250
+            UNION
+            SELECT {tablename.history[-1]}.id FROM {tablename.history[-1]} WHERE {tablename.history[-1]}.id = 750
+            """,
+        ],
+        [
+            "CREATE TEMPORARY TABLE {tablename.next} AS SELECT round(random() * 1000) id FROM generate_series(1, 1000000)",
+            "CREATE INDEX ON {tablename.history[-1]} (id)",
+            "SELECT {tablename.history[-1]}.id FROM {tablename.history[-1]} WHERE {tablename.history[-1]}.id IN (250, 750)",
         ],
     ],
 )
