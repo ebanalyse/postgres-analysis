83ed3ef0a1fba42ef5c54a0253fe83989bcdd084
Niels Lerche Sørensen <nielslerches@gmail.com>
2022-06-13 09:51:06 +0200

    flere tests

--- tests/test_prefix_list_indexes.py
+++ tests/test_prefix_list_indexes.py
@@ -6,7 +6,7 @@ from .utils import RandomTablenameGenerator, execute_and_measure
 
 
 @pytest.fixture
-def csv_buffer():
+def csv_buffer(request):
     prefix = ""
 
     rows = []
@@ -20,11 +20,16 @@ def csv_buffer():
                 prefix = suffix
             rows.append((prefix,))
     
-    with StringIO() as buffer:
-        writer = csv.writer(buffer)
-        writer.writerows(rows * 100_000)
-        buffer.seek(0)
-        yield buffer
+    buffer = StringIO()
+    writer = csv.writer(buffer)
+    writer.writerows(rows * 100_000)
+    buffer.seek(0)
+
+    @request.addfinalizer
+    def finalize():
+        buffer.close()
+
+    return buffer
 
 
 @pytest.mark.parametrize(
@@ -37,11 +42,34 @@ def csv_buffer():
         ],
     ],
 )
-def test_performance(postgres_connection, random_tablename: RandomTablenameGenerator, csv_buffer, statements: str | list[str]):
+def test_btree_performance(postgres_connection, random_tablename: RandomTablenameGenerator, csv_buffer, statements: str | list[str]):
     tablename = random_tablename.next
 
     with postgres_connection.cursor() as cursor:
         cursor.execute(f"CREATE TEMPORARY TABLE {tablename} (sections TEXT NOT NULL)")
+        cursor.copy_from(
+            csv_buffer,
+            tablename,
+        )
+
+    execute_and_measure(postgres_connection, random_tablename, statements)
+
+
+@pytest.mark.parametrize(
+    "statements",
+    [
+        "SELECT * FROM {tablename.history[-1]} WHERE sections @@ to_tsquery('english', '1500_:*')",
+        [
+            "CREATE INDEX ON {tablename.history[-1]} USING gin (sections)",
+            "SELECT * FROM {tablename.history[-1]} WHERE sections @@ to_tsquery('english', '1500_:*')",
+        ],
+    ],
+)
+def test_tsvector_performance(postgres_connection, random_tablename: RandomTablenameGenerator, csv_buffer, statements: str | list[str]):
+    tablename = random_tablename.next
+
+    with postgres_connection.cursor() as cursor:
+        cursor.execute(f"CREATE TEMPORARY TABLE {tablename} (sections TSVECTOR NOT NULL)")
         cursor.copy_from(
             csv_buffer,
             tablename,
